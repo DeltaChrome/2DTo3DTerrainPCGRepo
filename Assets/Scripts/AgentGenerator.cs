eba073f7c30be41124d7c81d4f3ca0095df3c1a8
nadernaseri <naseri.naader@gmail.com>
2020-12-06 21:10:26 -0500

    Agent New Algorithm

--- Assets/Scripts/AgentGenerator.cs
+++ Assets/Scripts/AgentGenerator.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class AgentGenerator : MonoBehaviour
 {
@@ -16,28 +17,112 @@ public class AgentGenerator : MonoBehaviour
 
      void Start()
      {
+/*Terrain terrainTemp = Terrain.activeTerrain;
+    var temp = terrainTemp.terrainData;
+             Vector3 scale=temp.heightmapScale;
+             int x=300;
+             int z= 200;
+  float hTemp = (float)temp.GetHeight((int)(x/scale.x),(int)(z/scale.z));
 
-    terrain = Terrain.activeTerrain;
-    int  tileValue = 25;
-    int   terrainSize = terrain.terrainData.detailWidth;
+    Debug.Log("##Test Heightmap"+hTemp);
+    Vector3 worldPosition = new Vector3(x,hTemp,z);
 
-    //Testing
-    List<Color32> colorList = new List<Color32>();
-    colorList.Add(new Color32(200,25,2,0));
-    Color32[] testColor = colorList.ToArray();
+  GameObject temp1 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+ float height = (float)(worldPosition.y*0.015);
+ worldPosition.y = (worldPosition.y)+height;
+   Debug.Log("##Test Y value"+ worldPosition.y);
+     temp1.transform.position = worldPosition;
+     */
+int pixelSize=10;
+//float terrainSize = terrain.terrainData.size.z;
+float terrainSize = 100;
+int x_tempValue=1;
+int z_tempValue=1;
+Color[, ] terrainTypeGrid = GetComponent<TerrainManager>().terrainTypeGrid;
 
+ //Debug.Log("##TEST terrain :" + terrainTypeGrid[1000,1000]);
+ 
+               //while(x_tempValue<terrainSize)
+                       {
+                       for(int x=x_tempValue; x<terrainSize; x+=30 )
+                       {
+                             for(int z=z_tempValue; z<terrainSize; z+=20 )
+                       {
+                                AgentGenAlgorithm(x,z);
+                       }
 
-        for (int x = 4; x < 50; x += tileValue)
-        {
+                       }
+                       }
 
-            AgentCreator(x,GettingColor(testColor));
-        }
+                       ColorChange();
+//AgentGenAlgorithm(Random.Range(1,900),Random.Range(1,900));
 
      }
       void Update()
 {
 
 }
+
+     void ColorChange()
+    {
+
+        Color[, ] terrainTypeGrid = GetComponent<TerrainManager>().terrainTypeGrid;
+
+        Texture2D texture = new Texture2D(128, 128);
+
+                GetComponent<Renderer>().material.mainTexture = texture;
+
+         Color color = Color.gray;
+         
+         texture.SetPixel(128,128,color);
+
+           texture.Apply();
+
+
+}
+
+void AgentGenAlgorithm(int xTile, int zTile)
+{
+   Color[, ] terrainTypeGrid = GetComponent<TerrainManager>().terrainTypeGrid;
+    int elementIndex = GettingColor(terrainTypeGrid[xTile,zTile]);
+          Element element = elements[elementIndex];
+
+               //   for (int i = 0; i < elements.Length; i++)
+                   // {
+                    if (element.CanPlace())
+                    {
+                            Vector3 position = new Vector3(xTile,heightValue(xTile,zTile),zTile);
+                            Vector3 offset = new Vector3(Random.Range(-0.75f, 1.75f), 0f, Random.Range(-0.75f, 1.75f));
+                            Vector3 rotation = new Vector3(Random.Range(0f, 5f), Random.Range(0, 360f), Random.Range(0f, 5f));
+                            
+                            //The first value of scale and yPosition should have the same amount
+                            Vector3 scale = Vector3.one * Random.Range(2.3f, 5.3f); 
+
+                            GameObject newElement = Instantiate(element.GetRandom());
+                            newElement.transform.SetParent(transform);
+                            newElement.transform.position = position + offset;
+                            newElement.transform.eulerAngles = rotation;
+                            newElement.transform.localScale = scale;
+                           // break;
+                    }
+              //  }
+}
+
+
+
+
+private float heightValue(int x, int z)
+{
+Terrain terrainTemp = Terrain.activeTerrain;
+    var temp = terrainTemp.terrainData;
+             Vector3 scale=temp.heightmapScale;
+  float hTemp = (float)temp.GetHeight((int)(x/scale.x),(int)(z/scale.z));
+    Vector3 worldPosition = new Vector3(x,hTemp,z);
+ float height = (float)(worldPosition.y*0.015);
+ //worldPosition.y = (worldPosition.y)+height;
+    return worldPosition.y;
+}
+
 
     [Obsolete]
     void AgentCreator(int tileVariable, int elementIndex)
@@ -92,22 +177,20 @@ GameObject newElement = Instantiate(element.GetRandom());
 }
 
 
-public int GettingColor(Color32[] rgba)
+public int GettingColor(Color32 rgba)
     {
 //determining the maximum color value
- Color32 maxValue = rgba.Max();
- //funcDensity();
- int elementName =0;
+ int elementIndex =0;
  //Debug.Log("TEST Color MaxValue: " + maxValue.ToString());
 
  // Debug.Log(Mathf.Max(rgba[0].r,rgba[0].g, rgba[0].b,rgba[0].a));
 
               // it determines the index value for determining the type of element
             List<int> colorListIndex = new List<int>();
-            colorListIndex.Add(rgba[0].r);
-            colorListIndex.Add(rgba[0].g);
-            colorListIndex.Add(rgba[0].b);
-            colorListIndex.Add(rgba[0].a);
+            colorListIndex.Add(rgba.r);
+            colorListIndex.Add(rgba.g);
+            colorListIndex.Add(rgba.b);
+            colorListIndex.Add(rgba.a);
             
 
          int maxIndexTest = colorListIndex.ToList().IndexOf(colorListIndex.Max());
@@ -115,19 +198,19 @@ public int GettingColor(Color32[] rgba)
  // Debug.Log("TEST Index : " + maxIndexTest.ToString());
          if (maxIndexTest == 0)
          {
-             elementName = 0; //If element is tree
+             elementIndex = 0; //If element is tree
          } else if (maxIndexTest == 1)
          {
-             elementName = 1; //If element is Grass
+             elementIndex = 1; //If element is Grass
          }else if (maxIndexTest == 3)
          {
-             elementName = 2; //If element is Rock
+             elementIndex = 2; //If element is Rock
          } else{
-             elementName = 3;
+             elementIndex = 3;
          }
 
           
-        return elementName;
+        return elementIndex;
     }
 
 public Tuple<int, int> GetMultipleValue()
@@ -174,7 +257,7 @@ public Tuple<int, int> GetMultipleValue()
         {
             
             //Debug.Log("Elements-Name:  " + name.ToString());
-            if (UnityEngine.Random.Range(0, 1) < density)
+            if (UnityEngine.Random.Range(1, 10) < density)
                 return true;
             else
                 return false;
